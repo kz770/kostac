bca6ff41d68ed9c0702454d0e248d1698de29fb8
유현진 <uhyeonjin0619@gmail.com>
2024-07-22 15:41:54 +0900

    fistcommit

---
+++ kosta_c/src/main/java/com/kosta/vo/MessageVO.java
@@ -0,0 +1,22 @@
+package com.kosta.vo;
+
+public class MessageVO {
+	String msg;
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public MessageVO(String msg) {
+		super();
+		this.msg = msg;
+	}
+	
+	public MessageVO() {
+		// TODO Auto-generated constructor stub
+	}
+}
